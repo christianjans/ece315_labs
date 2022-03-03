2c8eabad34f591ca61368aa303a0dfec141451a2
Christian Jans <cjjans@ualberta.ca>
2022-03-02 19:45:55 -0700

    Initial code for part 2

--- lab3/part 2/load_gen_main.c
+++ lab3/part 2/load_gen_main.c
@@ -99,22 +99,26 @@ static void TaskLoopCountProcessor(void *pvParameters){
     * Above statements explain the overview of this FreeRTOS task. Please add the code in the comments below to achieve the implementation.
     */
 
+
+    // TODO!!! Read the manual! There's something that has to be changed in the IDE.
+
+
     if(loop_count >= 500000){
       /**************************************************/
       //update the "delay" variable here to provide the delay of 90000
-
+      delay = pdMS_TO_TICKS(90000);
       /**************************************************/
     }
     if(loop_count >= 1000000){
       /**************************************************/
       //update the "delay" variable here to provide the delay of 120000
-
+      delay = pdMS_TO_TICKS(120000);
       /**************************************************/
     }
 
     /**************************************************/
     //write a line of code that increments the "loop_count" variable by 25000/50000
-
+    loop_count += 25000;
     /**************************************************/
     run = 0;
     xil_printf("\n\nCurrent loop count value = %d\n\n", loop_count);
@@ -136,6 +140,9 @@ static void TaskCpuLoadGen( void *pvParameters ){
     /*
     * Setup a for loop where this task is executing a simple bitwise complement operation for "loop_count" number of times on the variable "var"
     */
+    for (int i = 0; i < loop_count; i++) {
+      var ~= var;
+    }
     /*************************************************/
     vTaskDelay(1);
   }
@@ -156,6 +163,9 @@ static void TaskPrintRunTimeStats( void *pvParameters )
     * Print the results of the vTaskGetRunTimeStats() function onto the terminal
     * Declare the output variable that you want to use with the vTaskGetRunTimeStats() function
     */
+    char runtime_stats[40];  // Documentation says 40 bytes should be sufficient.
+    vTaskGetRunTimeStats(runtime_stats);
+    xil_printf("\n%s", runtime_stats);
     /*************************************************/
     run ++;
     vTaskDelay(delay);
